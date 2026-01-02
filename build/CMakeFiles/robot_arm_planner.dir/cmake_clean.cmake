138e786ca05ea421a9c03c8a21ea587100dcb7c9
Dan Fleming <flemingdaniel@rocketmail.com>
2026-01-02 15:10:02 +0100

    C++ project setup with CMake

---
+++ build/CMakeFiles/robot_arm_planner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_arm_planner.dir/src/main.cpp.o"
+  "CMakeFiles/robot_arm_planner.dir/src/main.cpp.o.d"
+  "robot_arm_planner"
+  "robot_arm_planner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_arm_planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Robot Arm Planner - Phase 0 setup OK\n";
+    return 0;
+}
