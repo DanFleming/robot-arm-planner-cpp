b8388814b825e03a2371d5a4c8e0693d043a010a
Dan Fleming <flemingdaniel@rocketmail.com>
2026-01-12 16:47:15 +0100

    adding jacobian test

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -50,4 +50,5 @@ set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/test_se2.dir/DependInfo.cmake"
   "CMakeFiles/test_se3.dir/DependInfo.cmake"
   "CMakeFiles/test_robot_arm_2d.dir/DependInfo.cmake"
+  "CMakeFiles/test_jacobian_2d.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/test_jacobian_2d.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_jacobian_2d.dir/src/test_jacobian_2d.cpp.o"
+  "CMakeFiles/test_jacobian_2d.dir/src/test_jacobian_2d.cpp.o.d"
+  "test_jacobian_2d"
+  "test_jacobian_2d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_jacobian_2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
