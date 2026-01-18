85f2c90a4dadf8ec444ab02f7683c64951f6379d
Dan Fleming <flemingdaniel@rocketmail.com>
2026-01-18 17:52:17 +0100

    adding visualisation

--- build/CMakeFiles/test_vectors.dir/flags.make
+++ build/CMakeFiles/test_vectors.dir/flags.make
@@ -4,7 +4,7 @@
 # compile CXX with /usr/bin/c++
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/home/dan/dev/robot-arm-planner-cpp/include
+CXX_INCLUDES = -I/usr/include/SDL2 -I/home/dan/dev/robot-arm-planner-cpp/include
 
 CXX_FLAGS = -std=gnu++17
 
---
+++ build/CMakeFiles/visualize.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visualize.dir/src/visualize.cpp.o"
+  "CMakeFiles/visualize.dir/src/visualize.cpp.o.d"
+  "visualize"
+  "visualize.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
